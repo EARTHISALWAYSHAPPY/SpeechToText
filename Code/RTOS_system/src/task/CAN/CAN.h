9afee78f2fd3dfae08f4f1aa788fe5e7978c7b36
EARTHISALWAYSHAPPY <eathdis@gmail.com>
2026-01-29 16:14:42 +0700

    add Domelight ID

--- Code/RTOS_system/src/task/CAN/CAN.h
+++ Code/RTOS_system/src/task/CAN/CAN.h
@@ -2,30 +2,36 @@
 #include <Arduino.h>
 #include "driver/twai.h"
 
-#define ID_HVAC    0x091
-#define ID_WINDOW  0x0A1
+#define ID_HVAC 0x091
+#define ID_WINDOW 0x0A1
+#define ID_DOMELIGHT 0x0A2
 
 // HVAC
-#define HVAC_AC_ON      0x01
-#define HVAC_AC_OFF     0x02
-#define HVAC_TEMP_UP    0x03
-#define HVAC_TEMP_DOWN  0x04
-#define HVAC_SET_TEMP   0x05
-#define HVAC_FAN_UP     0x06
-#define HVAC_FAN_DOWN   0x07
+#define HVAC_AC_ON 0x01
+#define HVAC_AC_OFF 0x02
+#define HVAC_TEMP_UP 0x03
+#define HVAC_TEMP_DOWN 0x04
+#define HVAC_SET_TEMP 0x05
+#define HVAC_FAN_UP 0x06
+#define HVAC_FAN_DOWN 0x07
 
 // Window
-#define WIN_OPEN        0x01
-#define WIN_CLOSE       0x02
-#define WIN_STOP        0x03
-#define WIN_SET_POS     0x04
+#define WIN_OPEN 0x01
+#define WIN_CLOSE 0x02
+#define WIN_STOP 0x03
+#define WIN_SET_POS 0x04
 
 // Window Position IDs
-#define W_ALL           0x00
-#define W_FL            0x01
-#define W_FR            0x02
-#define W_RL            0x03
-#define W_RR            0x04
+#define W_ALL 0x00
+#define W_FL 0x01
+#define W_FR 0x02
+#define W_RL 0x03
+#define W_RR 0x04
+
+// Domelight
+#define DL_OPEN 0x01
+#define DL_CLOSE 0x02
+
 struct CarData
 {
     // AC
@@ -38,6 +44,9 @@ struct CarData
     uint8_t win_fr = 0;
     uint8_t win_rl = 0;
     uint8_t win_rr = 0;
+
+    // Domelight
+    uint8_t dl_status = false;
 };
 extern volatile CarData currentCarState;
 void Can_init();
